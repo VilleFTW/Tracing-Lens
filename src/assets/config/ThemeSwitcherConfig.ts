811c9b152fdd15b214fb07d9aa1ddd13751652ce
VilleFTW <b.sorin189@gmail.com>
2022-07-28 22:05:01 +0200

    i18n: Added support for Finnish language

--- src/assets/config/ThemeSwitcherConfig.ts
+++ src/assets/config/ThemeSwitcherConfig.ts
@@ -10,6 +10,7 @@ export const countries = [
   { name: 'English', flag: '🇬🇧', value: 'en', dir: 'ltl' },
   { name: 'Nederlands', flag: '🇳🇱', value: 'nl', dir: 'ltl' },
   { name: 'Română', flag: '🇷🇴', value: 'ro', dir: 'ltl' },
+  { name: 'Suomi', flag: '🇫🇮', value: 'fi', dir: 'ltl' },
   { name: 'Español', flag: '🇪🇸', value: 'es', dir: 'ltl' },
   { name: 'عربي', flag: '🇸🇦', value: 'ar', dir: 'rtl' },
 ];
