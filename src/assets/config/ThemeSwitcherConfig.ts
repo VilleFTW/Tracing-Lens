7a388ffc239aaef9d1f1f056b4ae98fd4080597b
VilleFTW <b.sorin189@gmail.com>
2022-08-01 14:21:56 +0200

    i18n: Added support for German language

--- src/assets/config/ThemeSwitcherConfig.ts
+++ src/assets/config/ThemeSwitcherConfig.ts
@@ -9,8 +9,10 @@ export enum ColorBlindMode {
 export const countries = [
   { name: 'English', flag: '🇬🇧', value: 'en', dir: 'ltl' },
   { name: 'Nederlands', flag: '🇳🇱', value: 'nl', dir: 'ltl' },
+  { name: 'Deutsch', flag: '🇩🇪', value: 'de', dir: 'ltl' },
   { name: 'Română', flag: '🇷🇴', value: 'ro', dir: 'ltl' },
   { name: 'Suomi', flag: '🇫🇮', value: 'fi', dir: 'ltl' },
   { name: 'Español', flag: '🇪🇸', value: 'es', dir: 'ltl' },
+  { name: 'Lietuvos', flag: '🇱🇹', value: 'lt', dir: 'ltl' },
   { name: 'عربي', flag: '🇸🇦', value: 'ar', dir: 'rtl' },
 ];
